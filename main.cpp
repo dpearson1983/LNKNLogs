c8c9d64715cfc0978d9d581dab9bfabc8a040117
David Pearson <Entropy@ksu-wireless-226-217.campus.ksu.edu>
2017-06-30 09:01:56 -0500

    Add GPL license info

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+/* LNKNLogs v. 1.0
+ * David W. Pearson & Lado Samushia
+ * Copyright (C) 2016, 2017
+ * 
+ * This code creates simple lognormal mock catalogs with plane-parallel anisotropies. While these mocks
+ * are not ideal for matching surveys, they do produce measurable power spectrum multipoles with 
+ * subpercent level accuracy.
+ * 
+ * GPLv3:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
